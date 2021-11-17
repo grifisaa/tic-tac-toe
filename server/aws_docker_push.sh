02ea05df3d353386924d6ac430aa203113ec7137
Isaac Griffith <isaacgriffith@gmail.com>
2021-11-17 09:06:02 -0700

    added heroku push

--- server/aws_docker_push.sh
+++ server/aws_docker_push.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -e
 
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 565338450378.dkr.ecr.us-east-2.amazonaws.com
-docker tag tictactoe:latest 565338450378.dkr.ecr.us-east-2.amazonaws.com/tictactoe:latest
-docker push 565338450378.dkr.ecr.us-east-2.amazonaws.com/tictactoe:latest
+if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    if [ "$TRAVIS_BRANCH" == "develop" ] ; then
+        # Build and push
+        docker --version
+        pip install --user awscli
+        export PATH=$PATH:$HOME/.local/bin
+        eval $(aws ecr get-login --no-include-email --region us-east-2)
+        docker build -t "$LIVE_APP_NAME:latest" .
+        docker tag "$LIVE_APP_NAME:latest" "$AWS_URL/$LIVE_APP_NAME:$TRAVIS_COMMIT"
+        docker tag "$LIVE_APP_NAME:latest" "$AWS_URL/$LIVE_APP_NAME:latest"
+        docker push "$AWS_URL/$LIVE_APP_NAME:$TRAVIS_COMMIT"
+        docker push "$AWS_URL/$LIVE_APP_NAME:latest"
+    fi
+fi
